19b0379ab5757709f8c1f76df6c95d1c5ebca34d
Jebri Radhouane <jebri.radhouane@gmail.com>
2014-08-24 14:42:45 +0200

    modified:   .target/ApplicationTest.class 	new file:   .target/controllers/Application$Login.class 	modified:   .target/controllers/Application.class 	new file:   .target/controllers/Secured.class 	new file:   .target/views/group.scala.html 	new file:   .target/views/item.scala.html 	new file:   .target/views/login.scala.html 	modified:   .target/views/main.scala.html 	modified:   app/controllers/Application.java 	new file:   app/controllers/Secured.java 	new file:   app/views/group.scala.html 	new file:   app/views/item.scala.html 	new file:   app/views/login.scala.html 	modified:   app/views/main.scala.html 	modified:   conf/routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,10 +7,51 @@ import static play.data.Form.*;
 import views.html.*;
 import models.Project;
 import models.Task;
+import models.User;
 
 public class Application extends Controller {
-
+	@Security.Authenticated(Secured.class)
 	public static Result index() {
         return ok(index.render(Project.find.all(),Task.find.all()));
+	}
+	
+	public static Result login() {
+        return ok(
+            login.render(Form.form(Login.class))
+        );
+    }
+	
+	public static class Login {
+
+	    public String email;
+	    public String password;
+	    
+	    public String validate() {
+	        if (User.authenticate(email, password) == null) {
+	          return "Invalid user or password";
+	        }
+	        return null;
+	    }
+	}
+	
+	public static Result authenticate() {
+	    Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+	    if (loginForm.hasErrors()) {
+	        return badRequest(login.render(loginForm));
+	    } else {
+	        session().clear();
+	        session("email", loginForm.get().email);
+	        return redirect(
+	            routes.Application.index()
+	        );
+	    }
+	}
+	
+	public static Result logout() {
+	    session().clear();
+	    flash("success", "You've been logged out");
+	    return redirect(
+	        routes.Application.login()
+	    );
 	}
 }
